b6dcea89d8ad26e0cdf0b538dfdf5ccdcf62e458
Edriss572 <edriss_miakhel@yahoo.com>
2023-06-14 10:58:39 +0300

    This Assigment is done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+  
+  const handleOnFocuse = () => {
+    console.log('Good!')
+  }
+
+  const handleOnBlur = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  return (
+    <button onFocus={handleOnFocuse} onBlur={handleOnBlur}>
+      EyesOnMe
+    </button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
+import React from "react";
 
 function Keypad (){
+    const handleOnChange = () => {
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <input 
+            type="password"
+            placeholder="Enter password here..."
+            onChange={handleOnChange}
+        />
     )
 }
 
